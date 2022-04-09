bc64890333854e0f4e926fa2c22d7082afc8d3fb
Santiago Jaramillo <santiagojm84@gmail.com>
2022-04-09 08:33:14 -0500

    Creeación proyecto base

---
+++ PruebaSonarQube/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebaSonarQube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
